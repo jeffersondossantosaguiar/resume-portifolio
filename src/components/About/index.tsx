e00026d1e55abf663b12f5d6f79bee496626d628
Jefferson <jeffersonsantos.a@gmail.com>
2021-02-04 11:03:21 -0300

    Home

---
+++ src/components/About/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const About: React.FC = () => (
+  <>
+    <div id="about">
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+      <h1>About</h1>
+    </div>
+  </>
+)
+
+export default About
